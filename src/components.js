a2300d62c07b73aa9e3818da156289475fe8cb21
nicosis <nicosis@gmail.com>
2023-04-01 17:38:59 +0000

    iteration 7

--- src/components.js
+++ src/components.js
@@ -1,40 +1,158 @@
 import './components.css';
 
+//Iteration 1 | Component: IdCard
 export const IdCard = (props) => (
-  <div className='container'>
-    <img src={props.imgSrc} />
-    <div className='texto'>
-      <strong>First Name: </strong><span>{props.firstName}</span>
+  <div className="container">
+    <img className="idCard-img" src={props.imgSrc} />
+    <div className="texto">
+      <strong>First Name: </strong>
+      <span>{props.firstName}</span>
     </div>
     <div>
-      <strong>Last Name: </strong><span>{props.lastName}</span>
+      <strong>Last Name: </strong>
+      <span>{props.lastName}</span>
     </div>
     <div>
-      <strong>Gender: </strong><span>{props.gender}</span>
+      <strong>Gender: </strong>
+      <span>{props.gender}</span>
     </div>
     <div>
-      <strong>Height: </strong><span>{props.height}</span>
+      <strong>Height: </strong>
+      <span>{props.height}</span>
     </div>
     <div>
-      <strong>Birth: </strong><span>{props.birth}</span>
+      <strong>Birth: </strong>
+      <span>{props.birth}</span>
     </div>
   </div>
 );
 
-export const Greetings = (props) => {
-
-  if (props.lang === 'de') {
-    return (<div className='containerGreetings'>
-      <div>
-        <span style={{ padding: '5px' }}>Hallo {props.name}</span>
+//Iteration 2 | Component: Greetings
+export const Greetings = ({ lang, children }) => {
+  if (lang == 'de') {
+    return (
+      <div className="containerGreetings">
+        <span style={{ padding: '5px' }}>Hallo {children}</span>
       </div>
-    </div>)
-  } else if (props.lang === 'fr') {
-    return (<div className='containerGreetings'>
-      <div>
-        <span style={{ padding: '5px' }}>Bonjour {props.name}</span>
+    );
+  } else if (lang == 'fr') {
+    return (
+      <div className="containerGreetings">
+        <span style={{ padding: '5px' }}>Bonjour {children}</span>
       </div>
-    </div>)
+    );
   }
 };
-// export default Greetings;
+
+//Iteration 3 | Component: Random
+export const Random = ({ max, min }) => {
+  let random = Math.floor(Math.random() * (max - min + 1)) + min;
+  return (
+    <div className="containerGreetings">
+      Random value between {min} and {max} =&gt; {random}
+    </div>
+  );
+};
+
+//Iteration 4 | Component: BoxColor
+const decHex = (chanel) => {
+  const decimalNumber = chanel;
+  const hexNumber = decimalNumber.toString(16);
+  return hexNumber.length === 1 ? '0' + hexNumber : hexNumber;
+};
+
+export const BoxColor = (props) => {
+  return (
+    <div className="box" style={{ backgroundColor: `rgb(${props.r}, ${props.g}, ${props.b}` }}>
+      <span>
+        rgb({props.r},{props.g},{props.b})
+      </span>
+      <span>
+        #{decHex(props.r)}
+        {decHex(props.g)}
+        {decHex(props.b)}
+      </span>
+    </div>
+  );
+};
+
+//Iteration 5 | Component: CreditCard
+import visa from './assets/images/cc-visa.svg';
+import master from './assets/images/cc-mastercard.svg';
+
+export const CreditCard = (props) => {
+  let monthFull = props.expirationMonth.toString();
+  if (monthFull.length === 1) monthFull = '0' + monthFull;
+
+  let flag = props.type;
+  if (flag === 'Visa') flag = visa;
+  else if (flag === 'Master Card') flag = master;
+  return (
+    <div className="card" style={{ color: `${props.color}`, background: `${props.bgColor}` }}>
+      <div className="card-top">
+        <img className="card-icon" src={flag} alt="Logo" />
+      </div>
+      <div className="card-middle">{props.number}</div>
+      <div className="card-bottom">
+        <span>
+          Expires {monthFull}/{props.expirationYear} {props.bank}
+        </span>
+        <span>{props.owner}</span>
+      </div>
+    </div>
+  );
+};
+
+//Iteration 6 | Component: Rating
+import starEmpty from './assets/images/star-regular.svg';
+import starFull from './assets/images/star-solid.svg';
+
+const starDB = [
+  { '1': starEmpty, '2': starEmpty, '3': starEmpty, '4': starEmpty, '5': starEmpty },
+  { '1': starFull, '2': starEmpty, '3': starEmpty, '4': starEmpty, '5': starEmpty },
+  { '1': starFull, '2': starFull, '3': starEmpty, '4': starEmpty, '5': starEmpty },
+  { '1': starFull, '2': starFull, '3': starFull, '4': starEmpty, '5': starEmpty },
+  { '1': starFull, '2': starFull, '3': starFull, '4': starFull, '5': starEmpty },
+  { '1': starFull, '2': starFull, '3': starFull, '4': starFull, '5': starFull },
+];
+export const Rating = (props) => {
+  let result = {};
+  let propsRounded = Math.round(props.children);
+  if (propsRounded === 0) {
+    result = starDB[0];
+  } else if (propsRounded === 1) {
+    result = starDB[1];
+  } else if (propsRounded === 2) {
+    result = starDB[2];
+  } else if (propsRounded === 3) {
+    result = starDB[3];
+  } else if (propsRounded === 4) {
+    result = starDB[4];
+  } else if (propsRounded === 5) {
+    result = starDB[5];
+  }
+  return (
+    <div className="rating">
+      <img src={result[1]} alt="Star Full" />
+      <img src={result[2]} alt="Star Full" />
+      <img src={result[3]} alt="Star Full" />
+      <img src={result[4]} alt="Star Full" />
+      <img src={result[5]} alt="Star Full" />
+    </div>
+  );
+};
+
+//Iteration 7 | Component: DriverCard
+
+export const DriverCard = (props) => {
+  return (
+    <div style={{ backgroundColor: 'white', padding: '3px', width: '75%', marginLeft: '10px' }}>
+      <div className="drive">
+        <img className="idCard-img" src={props.img} />
+        <div>nombre</div>
+        <div>rating</div>
+        <div>car</div>
+      </div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -2,30 +2,101 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { IdCard, Greetings } from './components';
+import { IdCard, Greetings, Random, BoxColor, CreditCard, Rating, DriverCard } from './components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    Iteration 1 | Component: IdCard
     <IdCard
-      imgSrc='https://randomuser.me/api/portraits/men/52.jpg'
-      firstName='Nick'
-      lastName='Lenon'
-      gender='male'
+      imgSrc="https://randomuser.me/api/portraits/men/52.jpg"
+      firstName="Nick"
+      lastName="Lenon"
+      gender="male"
       height={1.78}
-      birth={new Date("1992-07-14").toDateString()}
+      birth={new Date('1992-07-14').toDateString()}
     />
     <IdCard
-      imgSrc='https://randomuser.me/api/portraits/women/44.jpg'
-      firstName='Obrien'
-      lastName='Lee'
-      gender='female'
+      imgSrc="https://randomuser.me/api/portraits/women/44.jpg"
+      firstName="Obrien"
+      lastName="Lee"
+      gender="female"
       height={1.72}
-      birth={new Date("1988-05-11").toDateString()}
+      birth={new Date('1988-05-11').toDateString()}
     />
-    <Greetings lang="de" name='Ludwing' />
-    <Greetings lang="fr" name='François' />
-    {/* <App /> */}
+    <br />
+    Iteration 2 | Component: Greetings
+    <Greetings lang="de">Ludwig</Greetings>
+    <Greetings lang="fr">François</Greetings>
+    <br />
+    Iteration 3 | Component: Random
+    <Random min={1} max={6} />
+    <Random min={2} max={5} />
+    <br />
+    Iteration 4 | Component: BoxColor
+    <BoxColor r={255} g={0} b={0} />
+    <BoxColor r={180} g={255} b={0} />
+    <br />
+    Iteration 5 | Component: CreditCard
+    <CreditCard
+      type="Visa"
+      number="0123456789018845"
+      expirationMonth={3}
+      expirationYear={2021}
+      bank="BNP"
+      owner="Maxence Bouret"
+      bgColor="#11aa99"
+      color="white"
+    />
+    <CreditCard
+      type="Master Card"
+      number="0123456789010995"
+      expirationMonth={3}
+      expirationYear={2021}
+      bank="N26"
+      owner="Maxence Bouret"
+      bgColor="#eeeeee"
+      color="#222222"
+    />
+    <CreditCard
+      type="Visa"
+      number="0123456789016984"
+      expirationMonth={12}
+      expirationYear={2019}
+      bank="Name of the Bank"
+      owner="Firstname Lastname"
+      bgColor="#ddbb55"
+      color="white"
+    />
+    <br />
+    Iteration 6 | Component: Rating
+    <Rating>0</Rating>
+    <Rating>1.49</Rating>
+    <Rating>1.5</Rating>
+    <Rating>3</Rating>
+    <Rating>4</Rating>
+    <Rating>5</Rating>
+    <br />
+    Iteration 7 | Component: DriverCard
+    <DriverCard
+      name="Travis Kalanick"
+      rating={4.2}
+      img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+      car={{
+        model: 'Toyota Corolla Altis',
+        licensePlate: 'CO42DE',
+      }}
+    />
+    <DriverCard
+      name="Dara Khosrowshahi"
+      rating={4.9}
+      img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+      car={{
+        model: 'Audi A3',
+        licensePlate: 'BE33ER',
+      }}
+    />
+    <br />
   </React.StrictMode>
 );
